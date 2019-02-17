163fa35c935f1b8a66d4d340766434198c49979b
Dalton-Gray <daltonuk@outlook.com>
2019-02-17 18:28:05 +0000

    added build_workers()

--- TerranBot.py
+++ TerranBot.py
@@ -1,11 +1,21 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import COMMANDCENTER, SCV
+
 
 
 class TerranBot(sc2.BotAI):
 	async def on_step(self, iteration):
 		await self.distribute_workers()
+		await self.build_workers()
+
+	async def build_workers(self):
+		for commandcenter in self.units(COMMANDCENTER).ready.noqueue:
+			if self.can_afford(SCV):
+				await self.do(commandcenter.train(SCV))	
+
+
 
 
 run_game(maps.get("AbyssalReefLE"), [
